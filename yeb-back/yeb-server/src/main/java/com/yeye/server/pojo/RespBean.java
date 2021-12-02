5393ed605013659c946b60d435d21bf37c1cdef3
ye-jingwen <2041443047@qq.com>
2021-12-02 16:32:55 +0800

    公共返回对象

---
+++ yeb-back/yeb-server/src/main/java/com/yeye/server/pojo/RespBean.java
@@ -0,0 +1,40 @@
+package com.yeye.server.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @program: yeb-back
+ * @description: 公共返回对象
+ * @author: YEYE
+ * @create: 2021-12-02
+ **/
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class RespBean {
+    private long code;
+    private String message;
+    private Object obj;
+
+    //成功返回结果
+    public static RespBean success(String message){
+        return new RespBean(200,message,null);
+    }
+
+    //成功返回结果
+    public static RespBean success(String message,Object obj){
+        return new RespBean(200,message,obj);
+    }
+
+    //失败返回结果
+    public static RespBean error(String message) {
+        return new RespBean(500, message, null);
+    }
+
+    //失败返回结果
+    public static RespBean error(String message,Object obj) {
+        return new RespBean(500, message, obj);
+    }
+}
